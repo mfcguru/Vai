4bfd05ee5ea610d556f79e3408a8e94cc2dbe446
Ronald Madrid <mfcguru@yahoo.com>
2021-07-10 01:00:09 +0800

    added efcore

--- Vai/Backend/Api/Startup.cs
+++ Vai/Backend/Api/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using Vai.Backend.Core.Entities;
 
 namespace Vai.Backend.Api
 {
@@ -22,9 +24,13 @@ namespace Vai.Backend.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddDbContextPool<VaiContext>(context => context
+                .UseLazyLoadingProxies()
+                .UseSqlServer(Configuration["ConnectionStrings:VaiConnectionString"])
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Vai/Backend/Vai.Backend.Core/Entities/VaiContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Vai.Backend.Core.Entities
+{
+    public partial class VaiContext : DbContext
+    {
+        public VaiContext()
+        {
+        }
+
+        public VaiContext(DbContextOptions<VaiContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Process> Processes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.\\sqlexpress;Database=Vai;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Process>(entity =>
+            {
+                entity.ToTable("Process");
+
+                entity.Property(e => e.Client)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.Priority)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Robot)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
