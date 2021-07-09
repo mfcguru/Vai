80f0e09c698239a5d92b9b77d7b0af1d520afdd8
Ronald Madrid <mfcguru@yahoo.com>
2021-07-10 01:18:20 +0800

    refactor

--- Vai/Backend/Vai.Backend.Core/Entities/VaiContext.cs
+++ Vai/Backend/Vai.Backend.Core/Entities/VaiContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
 #nullable disable
 
@@ -8,9 +6,6 @@ namespace Vai.Backend.Core.Entities
 {
     public partial class VaiContext : DbContext
     {
-        public VaiContext()
-        {
-        }
 
         public VaiContext(DbContextOptions<VaiContext> options)
             : base(options)
@@ -18,15 +13,6 @@ namespace Vai.Backend.Core.Entities
         }
 
         public virtual DbSet<Process> Processes { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.\\sqlexpress;Database=Vai;Trusted_Connection=True;");
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
