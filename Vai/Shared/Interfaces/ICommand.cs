efffe31ef966478ce0a786d40377ae5cb609883e
Ronald Madrid <mfcguru@yahoo.com>
2021-07-09 19:49:25 +0800

    added Command interface

---
+++ Vai/Shared/Interfaces/ICommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Vai.Shared.Results;
+
+namespace Vai.Shared.Interfaces
+{
+    public interface ICommand<TResult>
+    {
+        Task<CommandResult<TResult>> Execute();
+    }
+
+    public interface ICommand<TResult, TParams>
+    {
+        Task<CommandResult<TResult>> Execute(TParams parameters);
+    }
+
+    public interface ICommandList<TResult>
+    {
+        Task<CommandResult<List<TResult>>> Execute();
+    }
+
+    public interface ICommandList<TResult, TParams>
+    {
+        Task<CommandResult<List<TResult>>> Execute(TParams parameters);
+    }
+}
---
+++ Vai/Shared/Results/CommandResult.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Vai.Shared.Results
+{
+    public class CommandResult
+    {
+        public bool Success { get; set; }
+        public int StatusCode { get; set; }
+        public string Error { get; set; }
+    }
+
+    public class CommandResult<TData> : CommandResult
+    {
+        TData Data { get; set; }
+    }
+}
