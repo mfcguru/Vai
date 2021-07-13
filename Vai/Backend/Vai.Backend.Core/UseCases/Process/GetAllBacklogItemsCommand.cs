b3a0e28478ce1c66add4dd90e99ecc3ae7e1db05
SniperXZephyr <ronald.j.madrid@gmail.com>
2021-07-13 15:26:20 -0700

    implemented pagination

--- Vai/Backend/Vai.Backend.Core/UseCases/Process/GetAllBacklogItemsCommand.cs
+++ Vai/Backend/Vai.Backend.Core/UseCases/Process/GetAllBacklogItemsCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Vai.Backend.Core.Entities;
+using Vai.Backend.Core.Helpers;
 using Vai.Shared.Interfaces.Process;
 using Vai.Shared.Models;
 using Vai.Shared.Params;
@@ -22,19 +23,25 @@ namespace Vai.Backend.Core.UseCases.Process
         public async Task<CommandResult<List<GetAllBacklogItemsCommandModel>>> Execute(GetAllBacklogItemsCommandParams parameters)
         {
             var count = await context.Processes.CountAsync();
-            var totalPages = (count + parameters.PageSize - 1) / parameters.PageSize;
-
-            var result = await context.Processes
+            var items = await context.Processes
+                .Skip((parameters.Page - 1) * parameters.PageSize)
+                .Take(parameters.PageSize)
                 .Select(o => new GetAllBacklogItemsCommandModel
                 {
                     ProcessId = o.ProcessId,
                     Client = o.Client,
                     Robot = o.Robot,
                     TaskDescription = o.Description,
-                    TotalPages = totalPages
                 }).ToListAsync();
 
-            return new CommandResult<List<GetAllBacklogItemsCommandModel>> { Data = result };
+            var result = new PaginationHelper<GetAllBacklogItemsCommandModel>(items, count, parameters.Page, parameters.PageSize);
+
+            return new CommandResult<List<GetAllBacklogItemsCommandModel>> 
+            { 
+                Data = result, 
+                CurrentPage = result.PageIndex, 
+                TotalPages = result.TotalPages 
+            };
         }
     }
 }
