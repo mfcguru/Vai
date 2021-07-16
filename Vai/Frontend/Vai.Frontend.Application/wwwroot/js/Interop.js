5f3e2b5313f0d59068c7dd46d3e3f876655d458d
SniperXZephyr <ronald.j.madrid@gmail.com>
2021-07-15 22:46:28 -0700

    implemented js interops

---
+++ Vai/Frontend/Vai.Frontend.Application/wwwroot/js/Interop.js
@@ -0,0 +1,23 @@
+﻿var GLOBAL = {};
+GLOBAL.DotNetReference = null;
+GLOBAL.SetDotnetReference = function (pDotNetReference) {
+    GLOBAL.DotNetReference = pDotNetReference;
+};
+
+
+window.onresize = executeOnResize;
+
+function executeOnResize() {
+    callDotnetHandlePaginationDivOverflow();
+}
+
+function callDotnetHandlePaginationDivOverflow() {
+    GLOBAL.DotNetReference.invokeMethodAsync("HandlePaginationDivOverflow");
+}
+
+function isOverflowing(id) {
+    var el = document.getElementById(id);
+    var isOverflowing = el.clientHeight > 43;
+
+    return isOverflowing;
+}
