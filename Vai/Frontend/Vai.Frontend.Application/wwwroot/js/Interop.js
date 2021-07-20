bf851819395b3f17c9866050fc6ba89ad011561b
SniperXZephyr <ronald.j.madrid@gmail.com>
2021-07-20 01:46:15 -0700

    fixing some bugs in js interops and table pagination

--- Vai/Frontend/Vai.Frontend.Application/wwwroot/js/Interop.js
+++ Vai/Frontend/Vai.Frontend.Application/wwwroot/js/Interop.js
@@ -1,23 +1,40 @@
-﻿var BacklogReference = null;
+﻿var ActionReference = null;
+var BacklogReference = null;
+
+function SetActionReference(pDotNetReference) {
+    ActionReference = pDotNetReference;
+}
 
 function SetBacklogReference (pDotNetReference) {
     BacklogReference = pDotNetReference;
-};
-
+}
 
 window.onresize = executeOnResize;
 
 function executeOnResize () {
-    callDotnetHandleBacklogPaginationDivOverflow();
+    callBacklogHandlePaginationDivOverflow();
+    callActionHandlePaginationDivOverflow();
 }
 
-function callDotnetHandleBacklogPaginationDivOverflow () {
-    BacklogReference.invokeMethodAsync("HandlePaginationDivOverflow");
+function callActionHandlePaginationDivOverflow() {
+    ActionReference.invokeMethodAsync("HandleButtonSlideLeft");
 }
 
-function checkTablePaginationDivOverflow (id) {
+function callBacklogHandlePaginationDivOverflow () {
+    BacklogReference.invokeMethodAsync("HandleButtonSlideLeft");
+}
+
+function checkTablePaginationScrollLeft (id) {
     var el = document.getElementById(id);
     var isOverflowing = el.clientWidth < el.scrollWidth;
+
+    return isOverflowing;
+}
+
+function checkTablePaginationScrollRight(id) {
+    var el = document.getElementById(id);
+    var isOverflowing = el.clientHeight < el.scrollHeight;
+    console.log(el.clientHeight + " " + el.scrollHeight);
 
     return isOverflowing;
 }
