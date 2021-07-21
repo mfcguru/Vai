933d8010ba37b05af5fbc4522c415385f94d3a68
SniperXZephyr <ronald.j.madrid@gmail.com>
2021-07-20 22:53:07 -0700

    fixed bugs in table pagination

--- Vai/Frontend/Vai.Frontend.Application/wwwroot/js/Interop.js
+++ Vai/Frontend/Vai.Frontend.Application/wwwroot/js/Interop.js
@@ -28,13 +28,6 @@ function checkTablePaginationScrollLeft (id) {
     var el = document.getElementById(id);
     var isOverflowing = el.clientWidth < el.scrollWidth;
 
-    return isOverflowing;
-}
-
-function checkTablePaginationScrollRight(id) {
-    var el = document.getElementById(id);
-    var isOverflowing = el.clientHeight < el.scrollHeight;
-    console.log(el.clientHeight + " " + el.scrollHeight);
-
+    console.log(isOverflowing);
     return isOverflowing;
 }
